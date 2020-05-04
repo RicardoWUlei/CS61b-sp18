f94206f64628a17c21501f5a8f68b9b120d3166f
Xiake Zhu <zhuxiake2017@gmail.com>
2020-05-04 09:53:05 +0800

    Finish LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,16 @@ public class LeapYear {
         }
     }
 
+    /** Return true if the YEAR is leap year, namely 
+     *  divisible by 400 or divisible by 4 and not by 100 */
+    private static boolean isLeapYear(int year){
+        if(year % 400 == 0)
+            return true;
+        if(year % 4 == 0 && year % 100 != 0)
+            return true;
+        return false;
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
