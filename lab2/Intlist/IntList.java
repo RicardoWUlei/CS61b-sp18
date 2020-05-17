afaad87550aa01c176205c5c7b50497eedb012ed
Xiake Zhu <zhuxiake2017@gmail.com>
2020-05-17 20:31:11 +0800

    Test null argument for dcatenate method

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,6 +81,8 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
+        if(A==null)
+            return B;
         IntList result = A;
         while(A.rest != null){
             A = A.rest;
